bf39886a521cb1d89d3286c373cdc8183e8e1d09
Mariana <101410421+Placito@users.noreply.github.com>
2023-07-09 13:17:56 +0000

    reviews

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+//create the Footer component
+const Footer = () => {
+    return (
+        <div className="container-fluid p-2 bg-dark align-bottom text-light text-center">
+            <p>Copyright © Your Website 2023</p>
+        </div>
+    )
+};
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Jumbotron from "./jumbotron.jsx";
 import Navbar from "./navbar.jsx";
-import Card from "./card.jsx"
+import Card from "./card.jsx";
+import Footer from "./footer.jsx"
 
 //create your first component
 const Home = () => {
@@ -19,6 +20,7 @@ const Home = () => {
                 <Card description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, voluptatum iure ducimus laborum adipisci iusto itaque. Voluptates debitis sit labore."/>
             </div>
         </div>
+        <Footer />
 		</div>
     );
 };
