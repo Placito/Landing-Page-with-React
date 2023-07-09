925afc967d0f4bca30e96607f1270f64a667412d
Mariana <101410421+Placito@users.noreply.github.com>
2023-07-09 15:37:31 +0000

    some resome changes

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -3,13 +3,13 @@ import React from "react";
 //create the card component
 const Card = (props) => {
     return (
-    <div className="card m-3" style={{width: "18rem"}}>
+    <div className="card border m-3" style={{width: "18rem"}}>
         <img src="https://ro-camper.ro/imgs/inchiriat-camper-romania-rulota.jpg.png" className="card-img-top" />
         <div className="card-body text-center">
              <h5 className="card-title">Card title</h5>
              <p className="card-text">{props.description}</p>
          </div>
-        <div class="card-footer text-center pt-2">
+        <div className="card-footer text-center pt-2">
             <a href="#" className="btn btn-primary">Find Out More!</a>
         </div>
     </div> 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,27 @@
 import React from "react";
-import Jumbotron from "./jumbotron.jsx";
-import Navbar from "./navbar.jsx";
-import Card from "./card.jsx";
+import Jumbotron from "./jumbotron.jsx"
+import Navbar from "./navbar.jsx"
+import Card from "./card.jsx"
 import Footer from "./footer.jsx"
 
-//create your first component
+//create the main component
 const Home = () => {
     return (
-		<div>
-			<Navbar />
-        <div className="container">
-			<div>
-				<Jumbotron />
-			</div>
-            <div className="card-group">
+        <div className="container-fluid p-0 m-0">
+            <Navbar />
+            <div className="container text-center w-75">
+                <Jumbotron />
+                <div className="card-group mb-3">
                 <Card description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde illo culpa assumenda molestiae in."/>
-                <Card description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, voluptatum iure ducimus laborum adipisci iusto itaque. Voluptates debitis sit labore."/>
+                <Card description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, voluptatum iure ducimus laborum adipisci iusto itaque."/>
                 <Card description="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde illo culpa assumenda molestiae in."/>
-                <Card description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, voluptatum iure ducimus laborum adipisci iusto itaque. Voluptates debitis sit labore."/>
+                <Card description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, voluptatum iure ducimus laborum adipisci iusto itaque."/>
             </div>
+            </div>
+            <Footer />
         </div>
-        <Footer />
-		</div>
     );
 };
+
+
 export default Home;
