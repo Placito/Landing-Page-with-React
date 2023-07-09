4506ec43d882cc2b681eba43e8984179b4e55a57
Mariana <101410421+Placito@users.noreply.github.com>
2023-07-09 11:59:29 +0000

    component card corrections

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -5,11 +5,14 @@ const Card = (props) => {
     return (
     <div className="card m-3" style={{width: "18rem"}}>
         <img src="https://ro-camper.ro/imgs/inchiriat-camper-romania-rulota.jpg.png" className="card-img-top" />
-    <div className="card-body text-center">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">{props.description}</p>
-        <a href="#" className="btn btn-primary">Find Out More!</a>
-    </div>
-    </div> );
+        <div className="card-body text-center">
+             <h5 className="card-title">Card title</h5>
+             <p className="card-text">{props.description}</p>
+         </div>
+        <div class="card-footer text-center pt-2">
+            <a href="#" className="btn btn-primary">Find Out More!</a>
+        </div>
+    </div> 
+    );
 };
 export default Card;
