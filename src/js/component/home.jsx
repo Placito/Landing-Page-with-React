8720bc74ea11e0b0d88b7f870a6be83835538e98
Mariana <101410421+Placito@users.noreply.github.com>
2023-07-09 22:45:44 +0000

    syntax correction

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Jumbotron from "./jumbotron.jsx"
-import Navbar from "./navbar.jsx"
-import Card from "./card.jsx"
-import Footer from "./footer.jsx"
+import Jumbotron from "./jumbotron.jsx";
+import Navbar from "./navbar.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
 
 //create the main component
 const Home = () => {
